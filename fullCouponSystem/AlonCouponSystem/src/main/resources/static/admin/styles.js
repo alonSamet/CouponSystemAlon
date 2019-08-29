80f6000b4ee4ff8a3ff473528e01c29b44c4f7c2
alon <alonsamet@gmail.com>
2019-08-29 19:21:13 +0300

    .

--- fullCouponSystem/AlonCouponSystem/src/main/resources/static/admin/styles.js
+++ fullCouponSystem/AlonCouponSystem/src/main/resources/static/admin/styles.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "body {\r\n    /* background: url(\"https://images.pexels.com/photos/21859/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260\") no-repeat center center fixed; \r\n    -webkit-background-size: cover;\r\n    -moz-background-size: cover;\r\n    -o-background-size: cover;\r\n    background-size: cover;\r\n    min-height: 100%;\r\n    margin-left: 50px; margin-right: 50px; margin-top: 30px;\r\n    font-family: Verdana,Times New Roman,Arial,Sans-Serif; */\r\n}"
+module.exports = "body {\r\n    background: url(\"https://images.pexels.com/photos/21859/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260\") no-repeat center center fixed;\r\n    background-size: cover;\r\n    min-height: 100%;\r\n    margin-left: 50px; margin-right: 50px; margin-top: 30px;\r\n    font-family: Verdana,Times New Roman,Arial,Sans-Serif;\r\n}"
 
 /***/ }),
 
